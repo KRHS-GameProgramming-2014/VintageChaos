1e300bc8d6a560e199c00d7b88e3455709c78815
NathanCarbee <nathan.carbee@kearsarge.org>
2015-04-06 09:29:30 -0400

    new filesssssssssssssssssssssssssssssssssss........... scrubs

---
+++ CNYgame.py
@@ -0,0 +1,36 @@
+
+[file_prefs]
+final_new_line=true
+ensure_convert_new_lines=false
+strip_trailing_spaces=false
+replace_tabs=false
+
+[indentation]
+indent_width=4
+indent_type=0
+indent_hard_tab_width=8
+detect_indent=false
+detect_indent_width=false
+indent_mode=2
+
+[project]
+name=CNY2
+base_path=C:\\Users\\PLTW\\Documents\\Game Programming\\Nathan Carbee\\cny2
+description=
+
+[long line marker]
+long_line_behaviour=1
+long_line_column=72
+
+[files]
+current_page=2
+FILE_NAME_0=0;Python;0;EUTF-8;0;1;0;C%3A%5CUsers%5CPLTW%5CDocuments%5CGame%20Programming%5CNathan%20Carbee%5Ccny2%5Chealth.py;0;4
+FILE_NAME_1=2999;Python;0;EUTF-8;0;1;0;C%3A%5CUsers%5CPLTW%5CDocuments%5CGame%20Programming%5CNathan%20Carbee%5Ccny2%5CPlayerBall.py;0;4
+FILE_NAME_2=458;Python;0;EUTF-8;0;1;0;C%3A%5CUsers%5CPLTW%5CDocuments%5CGame%20Programming%5CNathan%20Carbee%5Ccny2%5CballDemo.py;0;4
+FILE_NAME_3=658;Python;0;EUTF-8;0;1;0;C%3A%5CUsers%5CPLTW%5CDocuments%5CGame%20Programming%5CNathan%20Carbee%5Ccny2%5CLevel.py;0;4
+FILE_NAME_4=1540;Python;0;EUTF-8;0;1;0;C%3A%5CUsers%5CPLTW%5CDocuments%5CGame%20Programming%5CNathan%20Carbee%5Ccny2%5CHUD.py;0;4
+FILE_NAME_5=1074;Python;0;EUTF-8;0;1;0;C%3A%5CUsers%5CPLTW%5CDocuments%5CGame%20Programming%5CNathan%20Carbee%5Ccny2%5CButton.py;0;4
+FILE_NAME_6=376;Python;0;EUTF-8;0;1;0;C%3A%5CUsers%5CPLTW%5CDocuments%5CGame%20Programming%5CNathan%20Carbee%5Ccny2%5CBlock.py;0;4
+FILE_NAME_7=2111;Python;0;EUTF-8;0;1;0;C%3A%5CUsers%5CPLTW%5CDocuments%5CGame%20Programming%5CNathan%20Carbee%5Ccny2%5CBall.py;0;4
+FILE_NAME_8=225;Python;0;EUTF-8;0;1;0;C%3A%5CUsers%5CPLTW%5CDocuments%5CGame%20Programming%5CNathan%20Carbee%5Ccny2%5CBackGround.py;0;4
+FILE_NAME_9=12;None;0;EUTF-8;0;1;0;C%3A%5CUsers%5CPLTW%5CDocuments%5CGame%20Programming%5CNathan%20Carbee%5Ccny2%5C.gitignore;0;4
---
+++ Level.py
@@ -0,0 +1,36 @@
+import pygame, sys, math
+
+from Block import Block
+
+
+class Level():
+	def __init__(self, screenSize, blockSize):
+		self.screenSize = screenSize
+		self.blockSize = blockSize
+		self.level = ""
+		
+	def loadLevel(self, level):
+		self.level = level
+		levelFile = "maps/" + level + ".lvl"
+		
+		f = open(levelFile, "r")
+		lines = f.readlines()
+		f.close()
+		
+		newlines = []
+		
+		for line in lines:
+			newline = ""
+			for c in line:
+				if c != "\n":
+					newline += c
+			newlines += [newline]
+			
+		lines = newlines
+		
+		for y, line in enumerate(lines):
+			for x, c in enumerate(line):
+				if c == "#":
+					Block("images/tiles/tile2.png", [x*self.blockSize,y*self.blockSize])
+			
+
