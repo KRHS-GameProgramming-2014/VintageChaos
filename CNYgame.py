f9654b4e3a108d7f1603f4dd047bcb903e0093e3
NathanCarbee <nathan.carbee@kearsarge.org>
2015-04-07 09:06:22 -0400

    here you go brother damian, i'm expecting money and dinner for this you greedy scrub

--- CNYgame.py
+++ CNYgame.py
@@ -23,14 +23,9 @@ long_line_behaviour=1
 long_line_column=72
 
 [files]
-current_page=2
-FILE_NAME_0=0;Python;0;EUTF-8;0;1;0;C%3A%5CUsers%5CPLTW%5CDocuments%5CGame%20Programming%5CNathan%20Carbee%5Ccny2%5Chealth.py;0;4
-FILE_NAME_1=2999;Python;0;EUTF-8;0;1;0;C%3A%5CUsers%5CPLTW%5CDocuments%5CGame%20Programming%5CNathan%20Carbee%5Ccny2%5CPlayerBall.py;0;4
-FILE_NAME_2=458;Python;0;EUTF-8;0;1;0;C%3A%5CUsers%5CPLTW%5CDocuments%5CGame%20Programming%5CNathan%20Carbee%5Ccny2%5CballDemo.py;0;4
-FILE_NAME_3=658;Python;0;EUTF-8;0;1;0;C%3A%5CUsers%5CPLTW%5CDocuments%5CGame%20Programming%5CNathan%20Carbee%5Ccny2%5CLevel.py;0;4
-FILE_NAME_4=1540;Python;0;EUTF-8;0;1;0;C%3A%5CUsers%5CPLTW%5CDocuments%5CGame%20Programming%5CNathan%20Carbee%5Ccny2%5CHUD.py;0;4
-FILE_NAME_5=1074;Python;0;EUTF-8;0;1;0;C%3A%5CUsers%5CPLTW%5CDocuments%5CGame%20Programming%5CNathan%20Carbee%5Ccny2%5CButton.py;0;4
-FILE_NAME_6=376;Python;0;EUTF-8;0;1;0;C%3A%5CUsers%5CPLTW%5CDocuments%5CGame%20Programming%5CNathan%20Carbee%5Ccny2%5CBlock.py;0;4
-FILE_NAME_7=2111;Python;0;EUTF-8;0;1;0;C%3A%5CUsers%5CPLTW%5CDocuments%5CGame%20Programming%5CNathan%20Carbee%5Ccny2%5CBall.py;0;4
-FILE_NAME_8=225;Python;0;EUTF-8;0;1;0;C%3A%5CUsers%5CPLTW%5CDocuments%5CGame%20Programming%5CNathan%20Carbee%5Ccny2%5CBackGround.py;0;4
-FILE_NAME_9=12;None;0;EUTF-8;0;1;0;C%3A%5CUsers%5CPLTW%5CDocuments%5CGame%20Programming%5CNathan%20Carbee%5Ccny2%5C.gitignore;0;4
+current_page=0
+FILE_NAME_0=99;Python;0;EUTF-8;0;1;0;C%3A%5CUsers%5CPLTW%5CDocuments%5CGame%20Programming%5CTrent%20Noordsij%5Cadventure%20swag%5CGame.py;0;4
+FILE_NAME_1=0;Python;0;EUTF-8;0;1;0;C%3A%5CUsers%5CPLTW%5CDocuments%5CGame%20Programming%5CTrent%20Noordsij%5Cadventure%20swag%5CPlayer.py;0;4
+FILE_NAME_2=616;Python;0;EUTF-8;0;1;0;C%3A%5CUsers%5CPLTW%5CDocuments%5CGame%20Programming%5CTrent%20Noordsij%5Cadventure%20swag%5CLevel.py;0;4
+FILE_NAME_3=0;Python;0;EUTF-8;0;1;0;C%3A%5CUsers%5CPLTW%5CDocuments%5CGame%20Programming%5CTrent%20Noordsij%5Cadventure%20swag%5CBackGround.py;0;4
+FILE_NAME_4=204;None;0;EUTF-8;0;1;0;C%3A%5CUsers%5CPLTW%5CDocuments%5CGame%20Programming%5CTrent%20Noordsij%5Cadventure%20swag%5Cmaps%5C1.lvl;0;4
